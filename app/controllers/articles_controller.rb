88b82cd96977300b7f320c4a4bdde00abc0e449a
tholmes741 <holmesiii.thomas@gmail.com>
2015-10-23 17:51:08 +0000

    tagging featur completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,4 +42,6 @@ class ArticlesController < ApplicationController
         flash.notice = "Article \'#{@article.title}\' Updated!"
         redirect_to article_path(@article)
     end 
+    
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+    
+    def tag_list
+        self.tags.map { |tag| tag.name }.join(', ')
+    end 
+    
+    def tag_list= (tag_string)
+        tag_names = tag_string.split(', ').map { |el| el.strip.downcase }.uniq
+        new_or_found_tags = tag_names.map{ |tag| Tag.find_or_create_by(name: tag)}
+        self.tags = new_or_found_tags
+    end 
 end
